9388910392c67d747b6b0856f6a0aaaaec7c1d07
DiarraMaim01 <maimouna_diarra@reseau.eseo.fr>
2026-02-22 17:59:32 +0100

    refactor: use @Value for JWT configuration

--- src/main/java/com/skillpass/backend/security/JwtService.java
+++ src/main/java/com/skillpass/backend/security/JwtService.java
@@ -17,14 +17,15 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
-    private static final long JWT_EXPIRATION = 86400000;
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> extraClaims = new HashMap<>();
-
         extraClaims.put("role", userDetails.getAuthorities());
-
         return generateToken(extraClaims, userDetails);
     }
 
@@ -33,17 +34,15 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
-
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
-
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -74,9 +73,8 @@ public class JwtService {
         return extractExpiration(token).before(new Date());
     }
 
-
     private Key getSigningKey() {
-        byte[] keyBytes = SECRET_KEY.getBytes();
+        byte[] keyBytes = secretKey.getBytes();
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
